a65209e01b02220436b3013a58c83e01f39113d6
Martim de Assis Lopes Cardeira <M.D.A.LopesCardeira@student.tudelft.nl>
2025-12-02 12:24:19 +0100

    day 2 part 2 finished

--- AdventOfCode/src/Day2.java
+++ AdventOfCode/src/Day2.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Day2 {
     public static void main(String[] args) throws IOException {
@@ -18,7 +20,7 @@ public class Day2 {
             long end = Long.parseLong(startAndEnd[1]);
             for(int i = 0; i <= end-start; i++) {
                 long current = start + i;
-                if(!isValid(Long.toString(start + i))){
+                if(!isValidPart2(Long.toString(start + i))){
                     sum += current;
                 }
             }
@@ -42,6 +44,31 @@ public class Day2 {
             return false;
         }
 
+        return true;
+    }
+
+    public static boolean isValidPart2(String str){
+        for(int i = 1; i <= str.length()/2; i++){
+            //the length of the substring is i+1
+            if(str.length() % i != 0) continue;
+            String pattern = str.substring(0, i);
+            int id = 0; //str pointer
+            int j = 0; //pattern pointer
+            boolean stoppedEarly = false;
+            while(id < str.length()){
+                if(j == pattern.length()) j = 0; //start the pattern again
+                if(str.charAt(id) != pattern.charAt(j)){
+                    stoppedEarly = true;
+                    break; //str not a concatenation of this particular pattern
+                }
+                id++;
+                j++;
+            }
+
+            if(!stoppedEarly){
+                return false; //str finishes with last digit of pattern, is a a concatenation, must be invalid
+            }
+        }
         return true;
     }
 }
